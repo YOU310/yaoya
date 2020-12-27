366a63ee812cf991aa8b71add09ef8119666ff0c
YOU <youmaya310@gmail.com>
2020-12-27 10:33:21 +0900

    ざっくり完成

--- yaoya.rb
+++ yaoya.rb
@@ -1,1 +1,51 @@
-puts
+params = {トマト: 100, きゅうり: 200, 玉ねぎ: 300, なす: 400}
+#キーを配列に格納
+item = params.keys
+#バリューを配列に格納
+price = params.values
+
+puts ""
+puts "いらっしゃいませ！商品を選んでください。"
+puts ""
+
+params.each.with_index(1) do |(key, value), i|
+  puts "#{i}.#{key}(#{value}円)"
+end
+
+puts ""
+#printにすると、改行がされないので
+print "商品の番号を選択　＞"
+
+#入力番号を整数で取得する
+select_num = gets.to_i
+
+puts ""
+puts "#{item[select_num-1]}ですね。何個買いますか？"
+
+puts ""
+
+print "個数を入力　＞"
+
+quantity = gets.to_i
+total_price = price[select_num-1] * quantity
+
+if quantity >= 5
+  total_price = (total_price * 0.9).floor
+
+  puts ""
+  puts "５個以上なので１０％割引になります！"
+  puts "#{item[select_num-1]}が#{quantity}個で、合計金額は#{total_price}円です。"
+  puts "お買い上げありがとうございました！"
+
+else
+  puts ""
+  puts "#{item[select_num-1]}が#{quantity}個で、合計金額は#{total_price}円です。"
+  puts "お買い上げありがとうございました！"
+
+end
+# puts "#{item[select_num-1]}が#{quantity}個で"
+# puts "合計金額は#{total_price}円です。"
+# puts "お買い上げありがとうございました！"
+
+
+
